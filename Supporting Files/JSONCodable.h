9482ede0914270b57810f258521e2b0c464cc313
Chris Griffiths <chris@codingtopher.co.uk>
2016-10-05 18:40:23 +0100

    Add support for watchOS

--- Supporting Files/JSONCodable.h
+++ Supporting Files/JSONCodable.h
@@ -6,15 +6,7 @@
 //
 //
 
-#import <Foundation/Foundation.h>
-//#import <JSONCodable/JSONCodable-Swift.h>
+@import Foundation;
 
-//! Project version number for JSONCodable.
 FOUNDATION_EXPORT double JSONCodable_VersionNumber;
-
-//! Project version string for JSONCodable.
 FOUNDATION_EXPORT const unsigned char JSONCodable_VersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <JSONCodable/PublicHeader.h>
-
-
